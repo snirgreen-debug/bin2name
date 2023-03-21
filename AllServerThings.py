b13f22904b75009fac2d625d21eb7ad94086808f
amitzri3 <61781115+amitzri3@users.noreply.github.com>
2023-03-21 09:22:31 +0200

    newer sever things

--- AllServerThings.py
+++ AllServerThings.py
@@ -6,6 +6,7 @@ import subprocess
 import json
 import time
 
+# This file should be placed in the HOME DIRECTORY of the project
 
 url = 'www.binary2name.com'
 port = 443
@@ -14,15 +15,10 @@ cert_path = '/etc/letsencrypt/live/www.binary2name.com/fullchain.pem'
 key_path = '/etc/letsencrypt/live/www.binary2name.com/privkey.pem'
 
 EXE_FILE_PATH = "./our_dataset/nero_ds/"
-LOF_FILE_PATH = "./nero/models_9999_log.txt"
+LOG_FILE_PATH = "./nero/models_9999_log.txt"
 
 
 class MyServer(CGIHTTPRequestHandler):
-    
-    def _predict_binary(self):
-        print(bin_file[:100])
-        # PredictWrapper.SaveBinFile(bin_file)
-
     def _parse_components(self):
         query = urlparse(self.path).query
         query_components = query.split("&")
@@ -35,10 +31,10 @@ class MyServer(CGIHTTPRequestHandler):
         timestr = time.strftime("%Y_%m_%d_%H_%M_%S")
         filename = EXE_FILE_PATH+timestr+".exe"
 
-        f = open("demofile.exe", "ab") # change to filename
+        f = open(filename, "ab")
         for i in range(content_length//1024):
             buffer = self.rfile.read(1024)
-            print(buffer[:10])
+            # print(buffer[:10])
             f.write(buffer)
 
         buffer = self.rfile.read(content_length%1024)
@@ -47,48 +43,36 @@ class MyServer(CGIHTTPRequestHandler):
         
         f.close()
         print("end of while")
-        my_dict = {"10016BA" : "my_func"}
-        body = json.dumps(my_dict).encode()
-        # self._parse_components()
-        # self._predict_binary()
+        # my_dict = {"10016BA" : "my_func"}
+        # body = json.dumps(my_dict).encode()
+        predicted_dict = Predict()
+        body = json.dumps(predicted_dict).encode()
         self.send_response(200)
         self.end_headers()
         self.wfile.write(body)
 
 
-class PredictWrapper:
-    def __init__(self):
-        self.name = "name"
-        self.LogParser = LogParser()
+def Predict():
+    predict_file_name = "./run_pipeline_for_predict.sh"
+    rc = subprocess.call(predict_file_name, shell=True)
 
-    def SaveBinFile(self, bin_file):
-        exe_folder_path = "our_dataset/nero_ds/"
-        with open(exe_folder_path, 'wb') as f:
-            f.write(bin_file)
+    # LOG_FILE_PATH
+    # path = "models_9999_log.txt"
+    address2name = Parse(LOG_FILE_PATH)
+    return address2name
 
-    def Predict(self):
-        predict_file_name = "./run_pipeline_for_predict.sh"
-        rc = subprocess.call(predict_file_name, shell=True)
-        
-        # Get the full Log Path
-        path = "models_9999_log.txt"
-        address2name = self.LogParser.Parse(path)
-        return address2name
 
-class LogParser:
-    def __init__(self):
-        self.name = "name"
-    def Parse(self, path):
-        address2name = {}
-        file = open(path, 'r')
-        lines = file.readlines()
-        for line in lines[1:]:
-            tokens = line.split(',')
-            address = tokens[1].split('@')[0]
-            predictedName = tokens[3]
-            if not (predictedName == ""):
-                address2name[address] = predictedName
-        return address2name
+def Parse(path):
+    address2name = {}
+    file = open(path, 'r')
+    lines = file.readlines()
+    for line in lines[1:]:
+        tokens = line.split(',')
+        address = tokens[1].split('@')[0]
+        predictedName = tokens[3]
+        if not (predictedName == ""):
+            address2name[address] = "P_"+predictedName.replace("*", "_")
+    return address2name
 
 
 if __name__ == '__main__':
